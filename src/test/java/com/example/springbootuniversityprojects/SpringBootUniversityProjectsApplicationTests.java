65cafec849ee655158623c297c7c41fb7846db05
AndreeaaBadea <andreea.badea@capgemini.com>
2022-03-28 17:50:34 +0300

    Created project

---
+++ src/test/java/com/example/springbootuniversityprojects/SpringBootUniversityProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootuniversityprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUniversityProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
